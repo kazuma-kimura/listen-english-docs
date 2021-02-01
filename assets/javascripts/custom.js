63d58d2dab9846444d8c10537831f32080e15514
kazuma-kimura <kazuma-kimura@users.noreply.github.com>
2021-02-01 14:20:30 +0000

    deploy: 13c64ace81aa5a4f4d4dfe5d579458cad60fe1a8

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,42 +1,22 @@
+const APP_WEBVIEW_USER_AGENT = 'app_webview';
 
 const header = document.querySelector('header');
 const footer = document.querySelector('footer');
 
-const isMobile = (userAgent) => {
-  alert(userAgent);
-  if (userAgent.indexOf('iphone') != -1) {
-    alert('iPhoneをお使いですね');
-  } else if (userAgent.indexOf('ipad') != -1) {
-    alert('iPadをお使いですね');
-  } else if (userAgent.indexOf('android') != -1) {
-    if (userAgent.indexOf('mobile') != -1) {
-      alert('androidのスマホをお使いですね');
-    } else {
-      alert('androidのタブレットをお使いですね');
-    }
+
+const isAppMobile = (userAgent) => {
+  if (userAgent == APP_WEBVIEW_USER_AGENT) {
+    return true;
   } else {
     return false;
   }
-  return true;
-}
-
-const addElement = (userAgent) => {
-  const element = document.createElement('div');
-  element.textContent = userAgent;
-  const target = document.querySelector('h1');
-  target.appendChild(element);
 }
 
 window.addEventListener('load', (event) => {
-  console.log("loadされたよー");
   const userAgent = window.navigator.userAgent.toLowerCase();
-  addElement(userAgent);
 
-  if (isMobile(userAgent)) {
-    alert('mobileだよー。ヘッダー、フッターを消すよ。');
+  if (isAppMobile(userAgent)) {
     header.style.display = 'none';
     footer.style.display = 'none';
-  } else {
-    alert('mobaileじゃないよー');
   }
 });
